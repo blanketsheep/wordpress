d19e82e958847fec47a53f5d8ff311e664bbe1bb
Shuma Yoshioka <s64.stdio@gmail.com>
2014-09-15 09:28:20 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '=> Install...'
-wp core install --allow-root --url="http://192.168.59.103" --title="Hachioji by Blanket Sheep" --admin_user="admin" --admin_password="admin" --admin_email="admin@example.com"
+wp core install --allow-root --url="http://hachioji.blanketsheep.comugi.co" --title="Hachioji by Blanket Sheep" --admin_user="admin" --admin_password="admin" --admin_email="admin@example.com"
 
 echo '=> Install Jetpack...'
 wp plugin install jetpack --allow-root
